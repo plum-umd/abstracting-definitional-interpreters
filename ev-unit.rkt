37f74216777d73ce5ac72157c98b672915f1968a
David Van Horn <dvanhorn@ccs.neu.edu>
2013-02-20 15:45:23 -0500

    Switch to racket from racket/unit for ev-unit.

--- ev-unit.rkt
+++ ev-unit.rkt
@@ -1,6 +1,5 @@
-#lang racket/unit
-(require racket/match
-         "bind-sig.rkt"
+#lang racket
+(require "bind-sig.rkt"
          "fail-sig.rkt"
 	 "delta-sig.rkt"
 	 "env-sig.rkt"
@@ -9,54 +8,56 @@
          "ev-sig.rkt"
          "syntax.rkt")
 
-(import unit^ bind^ δ^ env^ sto^ fail^)
-(export ev^)
+(provide ev@)
 
-(define-syntax do
-  (syntax-rules (←)
-    [(do b) b]
-    [(do x ← e . r)
-     (bind e (λ (x) (do . r)))]))
+(define-unit ev@
+  (import unit^ bind^ δ^ env^ sto^ fail^)
+  (export ev^)
 
-(define (ev e r) ;; E R -> [M Ans]
-  (match e
-    ['fail (fail)]
-    [(vbl x) (get r x)]
-    [(num n) (unit n)]
-    [(ifz e0 e1 e2)
-     (do v ← (rec e0 r)
-       (match v
-         [0 (rec e1 r)]
-         [n (rec e2 r)]))]
-    [(op1 o e0)
-     (do v ← (rec e0 r)
-       (δ o v))]
-    [(op2 o e0 e1)
-     (do v0 ← (rec e0 r)
-         v1 ← (rec e1 r)
-       (δ o v0 v1))]
-    [(ref e)
-     (do v ← (rec e r)
-       (new v))]
-    [(ubx e)
-     (do a ← (rec e r)
-       (ubox a))]
-    [(sbx e0 e1)
-     (do a ← (rec e0 r)
-         v ← (rec e1 r)
-       (sbox a v))]
-    [(lam x e)
-     (unit (cons (lam x e) r))]
-    [(lrc f (lam x e0) e)
-     (do a ← (ralloc f (cons (lam x e0) r))
-       (rec e (extend-env r f a)))]
-    [(app e0 e1)
-     (do v0 ← (rec e0 r)
-         v1 ← (rec e1 r)
-       (match v0
-         [(cons (lam x e) r0)
-          (do a ← (alloc (cons (lam x e) r0) v1)
-            (rec e (extend-env r0 x a)))]))]))
+  (define-syntax do
+    (syntax-rules (←)
+      [(do b) b]
+      [(do x ← e . r)
+       (bind e (λ (x) (do . r)))]))
+
+  (define (ev e r) ;; E R -> [M Ans]
+    (match e
+      ['fail (fail)]
+      [(vbl x) (get r x)]
+      [(num n) (unit n)]
+      [(lam x e) (unit (cons (lam x e) r))]
+      [(ifz e0 e1 e2)
+       (do v ← (rec e0 r)
+	 (match v
+	   [0 (rec e1 r)]
+	   [n (rec e2 r)]))]
+      [(op1 o e0)
+       (do v ← (rec e0 r)
+	 (δ o v))]
+      [(op2 o e0 e1)
+       (do v0 ← (rec e0 r)
+           v1 ← (rec e1 r)
+	 (δ o v0 v1))]
+      [(ref e)
+       (do v ← (rec e r)
+         (new v))]
+      [(ubx e)
+       (do a ← (rec e r)
+	 (ubox a))]
+      [(sbx e0 e1)
+       (do a ← (rec e0 r)
+           v ← (rec e1 r)
+         (sbox a v))]
+      [(lrc f (lam x e0) e)
+       (do a ← (ralloc f (cons (lam x e0) r))
+         (rec e (extend-env r f a)))]
+      [(app e0 e1)
+       (do v0 ← (rec e0 r)
+           v1 ← (rec e1 r)
+         (match v0
+           [(cons (lam x e) r0)
+	    (do a ← (alloc (cons (lam x e) r0) v1)
+	      (rec e (extend-env r0 x a)))]))])))
 
 
 (define (extend-env r x a)
