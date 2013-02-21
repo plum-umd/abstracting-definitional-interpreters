2a8855e4a39aa7f783e80c5ae8cf066d8100f20c
David Van Horn <dvanhorn@ccs.neu.edu>
2013-02-20 23:39:19 -0500

    Use racket/unit for env-unit.rkt.

--- env-unit.rkt
+++ env-unit.rkt
@@ -1,22 +1,20 @@
-#lang racket
-(require "signatures.rkt")
+#lang racket/unit
+(require racket/match
+         "signatures.rkt")
 
-(provide env@)
+(import unit^)
+(export env^)
 
-(define-unit env@
-  (import unit^)
-  (export env^)
+(define (get r x)
+  (unit (hash-ref r x)))
 
-  (define (get r x)
-    (unit (hash-ref r x)))
+(define (alloc f v)
+  (unit v))
 
-  (define (alloc f v)
-    (unit v))
-
-  (define (ralloc x v)
-    (match v
-      [(cons e r)
-       (define p (make-placeholder #f))
-       (define f (cons e (hash-set r x p)))
-       (placeholder-set! p f)
-       (unit (make-reader-graph f))])))
+(define (ralloc x v)
+  (match v
+    [(cons e r)
+     (define p (make-placeholder #f))
+     (define f (cons e (hash-set r x p)))
+     (placeholder-set! p f)
+     (unit (make-reader-graph f))]))
