a9ebee30e7f36785bf0cbe0d6c090e2a957e280b
David Van Horn <dvanhorn@ccs.neu.edu>
2013-02-23 14:59:25 -0500

    Reachable state semantics.

---
+++ units/eval-reachable-unit.rkt
@@ -0,0 +1,20 @@
+#lang racket/unit
+(require racket/match
+         racket/set
+	 "../signatures.rkt")
+
+(import ev^)
+(export eval^ unit^ bind^ rec^ err^)
+
+(define (eval e) (((rec e (hash)) (hash)) (set)))
+
+(define (((rec e r) s) t)
+  (((ev e r) s) (set-add t (list e r s))))
+
+(define (((unit v) s) t) (cons (cons v s) t))
+(define (((err) s) t) (cons (cons 'err s) t))
+(define (((bind a f) s) t)
+  (match ((a s) t)
+    [(cons (cons 'err s) t) (cons (cons 'err s) t)]
+    [(cons (cons v s) t)
+     (((f v) s) t)]))
