620bdd774978f53405058f5bb16dfeb2211d5c94
David Darais <david.darais@gmail.com>
2013-06-19 16:11:02 -0700

    added interact to makefile

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -6,11 +6,15 @@ import Fixpoints
 import Lang
 import qualified Lang.Lambda.BigStep.Symbolic as BS
 import qualified Lang.Lambda.SmallStep.Symbolic as SS
+import qualified Analyses.BigStep as BS
+import qualified Analyses.SmallStep as SS
 import Monads
 import StateSpace
 import Util
 import qualified Data.Text.Lazy.IO as T
 import Text.MPretty
+import Data.PartialOrder
+import Data.Lattice
 
 main :: IO ()
 main = do
--- haskell/StateSpace.hs
+++ haskell/StateSpace.hs
@@ -3,9 +3,11 @@ module StateSpace
   , module StateSpace.Concrete
   , module StateSpace.Env
   , module StateSpace.Store
+  , module StateSpace.Semantics
   ) where
 
 import StateSpace.CFA
 import StateSpace.Concrete
 import StateSpace.Env
 import StateSpace.Store
+import StateSpace.Semantics
