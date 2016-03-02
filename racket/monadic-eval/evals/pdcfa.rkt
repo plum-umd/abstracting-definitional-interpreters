de6911fe067d398413c8ccb9855b49d6b3660500
David Van Horn <dvanhorn@cs.umd.edu>
2016-03-01 23:25:42 -0500

    Use fixpoint combinator in PDCFA.

--- racket/monadic-eval/evals/pdcfa.rkt
+++ racket/monadic-eval/evals/pdcfa.rkt
@@ -9,26 +9,23 @@
 (define-values/invoke-unit/infer
   (link pdcfa@ ev-symbolic@ abs-δ@ sto-0cfa@))
 
-;; FIXME: use fix
+(define (((((co-ev e ρ) co-ev) σ) m1) m2)
+  (define ers (list e ρ σ))
+  (define anss (hash-ref m1 ers #false))  
+  (if anss
+      (cons anss m1)
+      (let ((m1′ (hash-set m1 ers (hash-ref m2 ers (set)))))
+	(match (((((ev e ρ) co-ev) σ) m1′) m2)
+	  [(cons anss m1)
+	   ;; XXX: why not union?
+	   (cons anss (hash-set m1 ers anss))]))))
 
 ;; eval : E ->_total [Setof Ans]
 ;; iterates ev until reaching a fixed point in the memo-table
 
-(define (eval e)
-  
-  (define ((((ev′ e ρ) σ) m1) m2)
-    (define ers (list e ρ σ))
-    (define anss (hash-ref m1 ers #false))  
-    (if anss
-        (cons anss m1)
-        (match (((((ev e ρ) ev′) σ)
-                 (hash-set m1 ers (hash-ref m2 ers (set)))) m2)                
-          [(cons anss m1)
-           (cons anss (hash-set m1 ers anss))])))
-  
-  
+(define (eval e)     
   (let loop ([m2 (hash)] [anss (set)])
-    (match ((((ev′ e (hash)) (hash)) (hash)) m2)
+    (match (((((fix co-ev) e (hash)) (hash)) (hash)) m2)
       [(and r (cons anss1 m1))
        (if (equal? r (cons anss m2))
 	   anss1
