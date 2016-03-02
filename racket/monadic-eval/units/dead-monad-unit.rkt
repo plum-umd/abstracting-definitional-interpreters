a112704c633adc24bebb992deb3a7b1cb0a73e43
David Van Horn <dvanhorn@cs.umd.edu>
2016-03-01 22:02:27 -0500

    Dead code monad.

---
+++ racket/monadic-eval/units/dead-monad-unit.rkt
@@ -0,0 +1,18 @@
+#lang racket/unit
+(require racket/match
+         "../signatures.rkt"
+         "../subexp.rkt")
+(import)
+(export unit^ bind^ #;run^)
+
+;; FIXME: can't write mrun here because need expr.
+#;
+(define (mrun M) ((M (hash)) (subexps e)))
+
+;; Same as trace monad
+(define (((unit v) s) t) (cons (cons v s) t))
+(define (((bind a f) s) t)
+  (match ((a s) t)
+    [(cons (cons 'err s) t) (cons (cons 'err s) t)]
+    [(cons (cons v s) t)
+     (((f v) s) t)]))
