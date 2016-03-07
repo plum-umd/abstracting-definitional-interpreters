748662c1b2bc75e04f09c5e47594fd4a30fe73f8
David Van Horn <dvanhorn@cs.umd.edu>
2016-03-07 18:55:44 -0500

    New lang interface.

--- racket/monadic-eval/lang.rkt
+++ racket/monadic-eval/lang.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require "syntax.rkt" "parser.rkt" "units.rkt")
+(require "syntax.rkt" "parser.rkt" "units.rkt" "units/ev-unit.rkt" "fix.rkt")
 (require (for-syntax racket/syntax))
 
 (require racket/stxparam)
@@ -19,10 +19,12 @@
 
 (define-syntax (module-begin stx)
   (syntax-case stx ()
-    [(_ l e ...)
+    [(_ (l ...) e0 e ...)
      (begin
-      (define linkage (syntax->datum #'l))
+      (define linkage (syntax->datum #'(link ev@ l ...)))
+      (define ev-exp (syntax->datum #'e0))
        #`(#%module-begin
-	  (define-values/invoke-unit/infer #,linkage)
+          (define-values/invoke-unit/infer #,linkage)
+          (define (eval e1) (mrun (#,ev-exp e1 (hash))))
           (set-box! run (λ (x) (eval (parse x))))
           ((unbox run) 'e) ...))]))
