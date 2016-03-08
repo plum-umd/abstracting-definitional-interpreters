30b11ba4af59c6a2dcc160eeea850d26fccbd7dc
Phil Nguyen <pcn@cs.umd.edu>
2016-03-07 20:16:43 -0500

    new signatures

---
+++ racket/new-world/signatures.rkt
@@ -0,0 +1,20 @@
+#lang racket/base
+(provide (all-defined-out))
+
+(define-syntax-rule
+  (define-signatures [sig : name ...] ...)
+  (begin (define-signature sig (name ...)) ...))
+
+(define-signatures
+  [ev^       : ev]
+  [monoid^   : O]
+  [monad^    : M mrun]
+  [env^      : ask-env local-env]
+  [store^    : get-store put-store]
+  [trace^    : tell-trace hijack-trace]
+  [cached^   : get-cache put-cache]
+  [state^    : rext ext find]
+  [alloc^    : alloc]
+  [ref^      : new sbox ubox]
+  [δ^        : δ truish?]
+  [symbolic^ : symbolic?])
