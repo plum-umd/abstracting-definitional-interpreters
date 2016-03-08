523e7d9c572f02208e16c205e71591d3a00d84bc
David Darais <david.darais@gmail.com>
2016-03-07 20:13:32 -0500

    fixed names and gave types in comments to fix-party

--- racket/new-world/fix-party.rkt
+++ racket/new-world/fix-party.rkt
@@ -1,18 +1,20 @@
+; caching : ∀mev. ((e → m(v)) → e → m(v)) → (e → m(v)) → e → m(v)
 (define (((caching ev) ev-caching) e)
   (do
     ρ ← ask-env
     σ ← get-store
     ς ≔ (list e ρ σ)
-    l$ ← get-l$
+    l$ ← get-lcache
     (if (hash-has-key? $ ς)
       (return (hash-ref $ ς))
       (do
-        g$ ← ask-g$
-        (update-l$ (λ (l$) (hash-set l$ ς (hash-ref g$ ς (set)))))
+        g$ ← ask-gcache
+        (update-lcache (λ (l$) (hash-set l$ ς (hash-ref g$ ς (set)))))
         v ← ((ev ev-caching) e)
-        (update-l$ (λ (l$) (hash-union l$ (hash ς (set v)))))
+        (update-lcache (λ (l$) (hash-union l$ (hash ς (set v)))))
         (return v)))))
 
+; memo-fix : ∀mev. ((e → m(v)) → e → m(v)) → e → m(v)
 (define ((memo-fix ev) e)
   ρ ← ask-env
   σ ← get-store
@@ -21,16 +23,18 @@
                 (do
                   (put-l$ (hash))
                   (put-store σ)
-                  (local-g$ g$ ((fix ev) e))
+                  (local-gcache g$ ((fix ev) e))
                   get-l$)))
   (return (hash-ref g$ ς)))
 
+; dead-fix : ∀mev. ((e → m(v)) → e → m(v)) → e → m(v)
 (define ((dead-fix ev) e)
   e₀ ← ask-program
   (put-dead-es e₀)
   ((fix ev) e))
 
-(define (mlfp f) ; f : a → m(a)
+; mlfp : ∀mkv. ((k ⇰ v) → m(k ⇰ v)) → m(k ⇰ v)
+(define (mlfp f)
   (define (loop x)
     (do
       x' ← (f x)
