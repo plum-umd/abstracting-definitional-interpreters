ecb866c92c8d33c818cd0aaf15258806123fa260
David Van Horn <dvanhorn@cs.umd.edu>
2016-03-09 16:16:13 -0500

    Write unit files once.

--- racket/monadic-eval/units.rkt
+++ racket/monadic-eval/units.rkt
@@ -1,10 +1,16 @@
 #lang racket/base
 
-(require
+(define-syntax-rule (require-provide f ...)
+  (begin
+    (require f ...)
+    (provide (all-from-out f ...))))
+
+(require-provide
  ;; evals
  "evals/eval-dead.rkt"
 
  ;; evs
+ "evs/ev.rkt"
  "evs/ev-base.rkt"
  "evs/ev-dead.rkt"
  "evs/ev-reach.rkt"
@@ -28,33 +34,3 @@
  "units/delta-symbolic.rkt"
  "units/delta-top.rkt"
  )
-
-(provide
- (all-from-out
-  ;; evals
-  "evals/eval-dead.rkt"
-
-  ;; evs
-  "evs/ev-base.rkt"
-  "evs/ev-dead.rkt"
-  "evs/ev-reach.rkt"
-  "evs/ev-symbolic.rkt"
-  "evs/ev-trace.rkt"
-  "evs/ev-compile.rkt"
-
-  ;; monads
-  "monad/monad-con.rkt"
-  "monad/monad-dead.rkt"
-  "monad/monad-reach.rkt"
-  "monad/monad-symbolic.rkt"
-  "monad/monad-trace.rkt"
-
-  ;; alloc
-  "units/alloc-con.rkt"
-
-  ;; delta
-  "units/delta-con.rkt"
-  "units/delta-pres.rkt"
-  "units/delta-symbolic.rkt"
-  "units/delta-top.rkt"
-  ))
