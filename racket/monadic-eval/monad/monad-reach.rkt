e6664ead5a77573cc5136cfbb6ad8135247ec28e
David Darais <david.darais@gmail.com>
2016-03-08 20:36:19 -0500

    forgot to save

--- racket/monadic-eval/monad/monad-reach.rkt
+++ racket/monadic-eval/monad/monad-reach.rkt
@@ -18,6 +18,11 @@
 ;; store^ impl:
 (define get-store (with-monad M get))
 (define put-store (with-monad M put))
+(define (update-store f)
+  (with-monad M
+    (do
+      σ ← get-store
+      (put-store (f σ)))))
 
 ;; reach^ impl:
 (define tell-reach   (with-monad M tell))
