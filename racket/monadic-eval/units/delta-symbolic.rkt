6b792e0a33060d668360d3a26eead4f0c34d8510
Phil Nguyen <pcn@cs.umd.edu>
2016-03-09 16:08:34 -0500

    ev-symbolic: reserve `N` (hack); implement `⊔` for `δ-symbolic`

--- racket/monadic-eval/units/delta-symbolic.rkt
+++ racket/monadic-eval/units/delta-symbolic.rkt
@@ -11,6 +11,8 @@
   (define (δ o . vs)
     (with-monad M
       (match* (o vs)
+        ; `N` is unrefinable, cannot build up symbolic value from it
+        [((not 'quotient) (list _ ... 'N _ ...)) (return 'N)]
         [('add1 (list (? number? n))) (return (add1 n))]
         [('add1 (list s)) (return `(add1 ,s))]
         [('+ (list (? number? n) (? number? m))) (return (+ n m))]
@@ -31,6 +33,8 @@
          (if (zero? n2)
              fail
              (return `(quotient ,s1 ,n2)))]
+        [('quotient (list _ 'N))
+         (mplus (return 'N) fail)]
         [('quotient (list s1 s2))
          (do b ← (truish? s2) ; relies on `s2`'s range being just numbers
            (if b fail (return `(quotient ,s1 ,s2))))]
@@ -38,7 +42,10 @@
          (do b ← (truish? v)
            (return (if b 1 0)))])))
 
-  (define (⊔ v . _) v)
+  (define (⊔ v . vs)
+    (foldl (λ (vₙ lub) (if equal? vₙ lub) vₙ 'N)
+           v
+           vs))
 
   (define (truish? v)
     (with-monad M
