aab0777d2f90cdad67264da188bf5d16ac2190bc
David Van Horn <dvanhorn@cs.umd.edu>
2016-03-12 19:32:10 -0500

    Use define-monad instead of with.

--- racket/monadic-eval/monad/monad.rkt
+++ racket/monadic-eval/monad/monad.rkt
@@ -3,26 +3,34 @@
          "../transformers.rkt"
 	 "../map.rkt")
 (import)
-(export monad^ menv^ mstore^)
+(export monad^ menv^ mstore^ state^)
 
 ;; monad^ impl:
 
 ;; M := ρ → σ → (a × σ)
 (define M (ReaderT (StateT #f (FailT ID))))
+(define-monad M)
 
 ;; mrun : (M a) [→ ρ [→ θ [→ σ]]] → a × σ
 (define (mrun m [ρ₀ ∅] [σ₀ ∅])
   (run-StateT σ₀ (run-ReaderT ρ₀ m)))
 
 ;; env^ impl:
-(define ask-env   (with-monad M ask))
-(define local-env    (with-monad M local))
+(define ask-env ask)
+(define local-env local)
 
 ;; store^ impl:
-(define get-store (with-monad M get))
-(define put-store (with-monad M put))
+(define get-store get)
+(define put-store put)
 (define (update-store f)
-  (with-monad M
-    (do
-      σ ← get-store
-      (put-store (f σ)))))
+  (do σ ← get-store
+      (put-store (f σ))))
+
+
+;; In-lined from metafunctions
+
+(define (find a)
+  (do σ ← get-store
+      (return (σ a))))
+
+(define (ext a v) (update-store (λ (σ) (σ a v))))
