37ce0324eaa74efadb22da2a94a9aad2944c9f61
David Van Horn <dvanhorn@cs.umd.edu>
2016-03-14 06:36:47 -0400

    Elim quasiquote patterns.

--- racket/monadic-eval/metafuns/delta-abs.rkt
+++ racket/monadic-eval/metafuns/delta-abs.rkt
@@ -8,21 +8,21 @@
   (import monad^)
   (export δ^)
 
+  (define-monad M)
+
   (define (δ . ovs)
-    (with-monad M
-      (match ovs
-        [`(add1 ,_)   (return 'N)]
-        [`(sub1 ,_)   (return 'N)]
-        [`(+ ,_ ,_) (return 'N)]
-        [`(- ,_ ,_) (return 'N)]
-        [`(* ,_ ,_) (return 'N)]
-        [`(quotient ,_ ,(? number? n₂))
-         (if (zero? n₂) fail (return 'N))]
-        [`(quotient ,_ ,_)
-         (mplus (return 'N) fail)])))
+    (match ovs
+      [(list 'add1 n)  (return 'N)]
+      [(list 'sub1 n)  (return 'N)]
+      [(list '+ n0 n1) (return 'N)]
+      [(list '- n0 n1) (return 'N)]
+      [(list '* n0 n1) (return 'N)]
+      [(list 'quotient n0 (? number? n1))
+       (if (= 0 n1) fail (return 'N))]
+      [(list 'quotient n0 n1)
+       (mplus (return 'N) fail)]))
 
   (define (truish? v)
-    (with-monad  M
-      (match v
-        ['N (mplus (return #t) (return #f))]
-        [_  (return (zero? v))]))))
+    (match v
+      ['N (mplus (return #t) (return #f))]
+      [_  (return (zero? v))])))
