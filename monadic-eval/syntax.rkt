5dfb654581af38c6164c4b2c5ecab55f8ab9a9d1
David Van Horn <dvanhorn@cs.umd.edu>
2016-03-21 15:42:29 -0400

    Elim pp.

--- monadic-eval/syntax.rkt
+++ monadic-eval/syntax.rkt
@@ -26,20 +26,3 @@
 (struct srf (e0 e1)    #:symbolic)
 
 (struct sym (s)        #:symbolic)
-
-(define (pp s)
-  (match s
-    ['err "err"]
-    [(app (lam x b) a) (format "(let ~a ~a ~a)" x (pp a) (pp b))]
-    [(app e0 e1)       (format "(~a ~a)" (pp e0) (pp e1))]
-    [(lam x  e1)       (format "(λ (~a) ~a)" x (pp e1))]
-    [(vbl x)           (format "~a" x)]
-    [(num n)           (format "~a" n)]
-    [(ifz e0 e1 e2)    (format "(if0 ~a ~a ~a)" (pp e0) (pp e1) (pp e2))]
-    [(op1 op e)        (format "(~a ~a)" op (pp e))]
-    [(op2 op e0 e1)    (format "(~a ~a ~a)" op (pp e0) (pp e1))]
-    [(lrc f x e)       (format "(rec ~a ~a ~a)" f (pp x) (pp e))]
-    [(ref e)           (format "(ref ~a)" (pp e))]
-    [(drf e)           (format "(! ~a)" (pp e))]
-    [(srf e0 e1)       (format "(~a := ~a)" (pp e0) (pp e1))]
-    [(sym s)           (format "'~a" s)]))
