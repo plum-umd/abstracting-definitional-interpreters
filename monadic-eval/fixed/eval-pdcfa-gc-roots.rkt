ac5d1f69bb28d9b9e6b6f0338cc2f0d3998cae1c
David Van Horn <dvanhorn@cs.umd.edu>
2016-04-02 19:52:10 -0400

    Fix up other GC evals to elim ev-gc.

--- monadic-eval/fixed/eval-pdcfa-gc-roots.rkt
+++ monadic-eval/fixed/eval-pdcfa-gc-roots.rkt
@@ -4,7 +4,7 @@
 (provide eval)
 
 (define-values/invoke-unit/infer
-  (link monad-pdcfa-gc@ state-nd@ alloc-0cfa@ δ-abs@ ev-gc@ ev-collect@ ev-cache-roots@ eval-coind-roots@))
+  (link monad-pdcfa-gc@ state-nd@ alloc-0cfa@ δ-abs@ ev@ ev-roots@ ev-collect@ ev-cache-roots@ eval-coind-roots@))
 
 (define (eval e)
-  (mrun ((eval-coind (fix (ev-cache (ev-collect ev)))) e)))
+  (mrun ((eval-coind (fix (ev-cache (ev-collect (ev-roots ev))))) e)))
